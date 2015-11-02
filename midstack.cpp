fa38721a00a7723e962b279aafc6d5292115d7e7
hsiangkuotang <hsiangkuo.tang@gmail.com>
2015-11-02 08:22:36 -0800

    Create midstack.cpp

---
+++ midstack.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* prev;
+    Node(int x) {
+        val = x;
+        next = prev = NULL;
+    }
+};
+
+class MidStack {
+public:
+    MidStack() {
+        head = mid = NULL;
+        size = 0;
+    }
+    void push(int x) {
+        Node* curr = new Node(x);
+        if (head != NULL) {
+            curr->next = head;
+            head->prev = curr;
+        }
+        head = curr;
+        size++;
+        if (size == 1) {
+            mid = head;
+        }
+        else if (size & 1) {
+            mid = mid->prev;            
+        }
+    }
+    int pop() {
+        if (head == NULL) {
+            return -1;
+        }
+        Node* delNode = head;
+        int res = delNode->val;
+        head = head->next;
+        size--;
+        if (size == 0) {
+            mid = NULL;
+        }
+        else if (!(size & 1)) {
+            mid = mid->next;
+        }
+        delete delNode;
+        return res;
+    }
+    int findMiddle() {
+        return mid->val;
+    }
+private:
+    Node* head;
+    Node* mid;
+    int size;
+};
+
+int main() {
+	// your code goes here
+	MidStack st;
+	st.push(11);
+	st.push(22);
+	st.push(33);
+	st.push(44);
+	st.push(55);
+	st.push(66);
+	st.push(77);
+
+    cout << "Item popped is " << st.pop() << endl;
+    cout << "Item popped is " << st.pop() << endl;
+    cout << "Middle Element is " << st.findMiddle() << endl;
+	return 0;
+}
