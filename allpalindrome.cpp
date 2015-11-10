4f29f1cc9a2822945cb26ad1a8ad5f39c2d29a69
hsiangkuotang <hsiangkuo.tang@gmail.com>
2015-11-09 16:57:59 -0800

    Update allpalindrome.cpp

--- allpalindrome.cpp
+++ allpalindrome.cpp
@@ -5,24 +5,41 @@ find all palindrome strings by deleting any letter from a given string
 #include <iostream>
 #include <string>
 #include <unordered_set>
-#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     unordered_set<string> allPalindrome(string s) {
+        return palinHelper(s, 0, s.size() - 1);
+    }
+private:
+    unordered_set<string> palinHelper(string s, int left, int right) {
         unordered_set<string> res;
-        if (s.empty()) return res;
-        if (s.size() == 1) {
-            res.insert(s);
+        if (left > right) return res;
+        if (left == right) {
+            res.insert(s.substr(left, 1));
             return res;
         }
-        vector<vector<bool>> f(s.size(), vector<bool>(s.size(), false));
-        for (int i = s.size() - 1; i >= 0; i--) {
-            for (int j = i; j < s.size(); j++) {
-                f[i][j] = (s[i] == s[j] && (j - i < 2 || f[i + 1][j - 1]));
-                if (f[i][j]) res.insert(s.substr(i, j - i + 1));
+        for (int i = left; i <= right; i++) {
+            for (int j = right; j >= i; j--) {
+                if (s[i] == s[j]) {
+                    unordered_set<string> next = palinHelper(s, i + 1, j - 1);
+                    for (auto it = next.begin(); it != next.end(); it++) {
+                        res.insert(*it);
+                        res.insert(s[i] + *it + s[j]);
+                    }
+                }
+                else {
+                    unordered_set<string> next1 = palinHelper(s, i + 1, j);
+                    for (auto it = next1.begin(); it != next1.end(); it++) {
+                        res.insert(*it);
+                    }
+                    unordered_set<string> next2 = palinHelper(s, i, j - 1);
+                    for (auto it = next2.begin(); it != next2.end(); it++) {
+                        res.insert(*it);
+                    }
+                }
             }
         }
         return res;
@@ -32,7 +49,7 @@ public:
 int main() {
 	// your code goes here
 	Solution s;
-	string t = "abcbabcba";
+	string t = "abcbba";
 	unordered_set<string> res = s.allPalindrome(t);
 	for (auto it = res.begin(); it != res.end(); it++) {
 	    cout << *it << endl;
